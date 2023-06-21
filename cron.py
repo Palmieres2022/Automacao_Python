b234d495f125e61d8b4266a3fb3b4de0123b16e5
Palmieres Mota <palmierres22@gmail.com>
2023-06-21 14:28:13 -0300

    Criado script de organização de arquivos

---
+++ cron.py
@@ -0,0 +1,20 @@
+import os
+
+types = ['jpeg', 'zip']
+
+base_path = os.path.expanduser('~')
+path = os.path.join(base_path, 'Downloads')
+
+cwd = os.chdir(path)
+
+full_list = os.listdir(cwd)
+for type_ in types:
+    if type_ in not in os.listdir():
+        os.mkdir(type_)
+
+for file in full_list:
+    for type_ in types:
+        if ',' + type_ in file:
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path, type_, file)
+            os.replace(old_path, new_path)
